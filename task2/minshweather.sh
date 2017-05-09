7c0d05576914c8e07adfefbe307b34793d6205f1
Roman Toropov <toropov.r@gmail.com>
2017-05-09 22:52:11 +0300

    Add task 2

---
+++ task2/minshweather.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function get_temp(){
+temp=&(wget --quiet -O - pogoda.by | iconv -f cp1251 | grep "Температура воздуха" | awk '{ print $6 }' | sed -r 's/<.+//')
+echo -n $temp
+}
+
+read line < "config.ini"
+while true; do get_temp; sleep $line; done;
+
+
