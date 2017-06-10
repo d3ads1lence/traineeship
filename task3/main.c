f04d5c1bb04b4dfeb8f51dc5db8db13740d7a054
Roman Toropov <toropov.r@gmail.com>
2017-06-10 22:31:41 +0300

    Add semaphore

--- task3/main.c
+++ task3/main.c
@@ -1,24 +1,48 @@
 /* main.c */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <semaphore.h>
+#include <errno.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
+sem_t *sem;
+char SEM_NAME[]= "test";
 void pipe_writer (FILE* stream);
 void pipe_reader(FILE* stream);
 
 int main() {
 	int fds[2];
-	pid_t pid;
+	pid_t pid, pid2;
 	pipe(fds);
+
+	sem = sem_open(SEM_NAME,O_CREAT,0644,0);
+	 if(sem == SEM_FAILED)
+    {
+		perror("unable to create semaphore");
+		sem_unlink(SEM_NAME);
+		exit(-1);
+    }
 	
 	pid = fork();
 	if (pid == (pid_t)0) {
+		pid2 = fork();
+		if (pid2 == 0){
+			while(1){
+				if (sem_wait(sem) == 0){
+					printf("> 100\n");
+				}
+			}
+		} else {
 		FILE *stream;
 		close(fds[1]);
 		stream = fdopen(fds[0], "r");
 		pipe_reader(stream);
 		close(fds[0]);
+		}
 	} else {
 		FILE* stream;
 		close(fds[0]);
@@ -49,8 +73,15 @@ void pipe_reader(FILE* stream)
 {
 	char buffer[10];
 	while (!ferror(stream) && fgets(buffer, sizeof (buffer), stream) != NULL) {
-		printf("This number was readed from pipe: ");
-		fputs(buffer, stdout);
-		printf("\n");
+		int number = atoi(buffer);
+		if (number > 100) {
+			// sem_wait(&sem);
+			// printf("> 100\n");
+			sem_post(sem);
+		} else {
+			printf("< 100\n");
+		}
+		// printf("This number was readed from pipe: %d\n", number);
+		// fputs(buffer, stdout);
 	}
 }
