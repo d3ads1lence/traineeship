2da5a47294b239b3005243c933258ff3a0e5c044
Roman Toropov <toropov.r@gmail.com>
2017-06-11 15:17:49 +0300

    Add shared memory

--- task3/main.c
+++ task3/main.c
@@ -8,8 +8,12 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
 
 sem_t *sem;
+key_t key = 1000;
+int shmid;
 char SEM_NAME[]= "test";
 void pipe_writer (FILE* stream);
 void pipe_reader(FILE* stream);
@@ -18,6 +22,8 @@ int main() {
 	int fds[2];
 	pid_t pid, pid2;
 	pipe(fds);
+  	
+  	
 
 	sem = sem_open(SEM_NAME,O_CREAT,0644,0);
 	 if(sem == SEM_FAILED)
@@ -31,24 +37,41 @@ int main() {
 	if (pid == (pid_t)0) {
 		pid2 = fork();
 		if (pid2 == 0){
+			/* Process C */
+			 //create the shared memory segment with this key
+			shmid = shmget(key,getpagesize(),0666);
+			if(shmid<0)
+			{
+				perror("reader:failure in shmget");
+				exit(-1);
+			}
+
+			//attach this segment to virtual memory
+			char* shared_memory_c = shmat(shmid,NULL,0);
 			while(1){
 				if (sem_wait(sem) == 0){
-					printf("> 100\n");
+					printf("%s\n", shared_memory_c);
 				}
+			/* Process C */
 			}
 		} else {
+		/* Process B */
+
 		FILE *stream;
 		close(fds[1]);
 		stream = fdopen(fds[0], "r");
 		pipe_reader(stream);
 		close(fds[0]);
+		/* Process B */
 		}
 	} else {
+		/* Process A */
 		FILE* stream;
 		close(fds[0]);
 		stream = fdopen(fds[1], "w");
 		pipe_writer(stream);
 		close(fds[1]);
+		/* Process A */
 	}
 return 0;
 }
@@ -72,16 +95,22 @@ void pipe_writer (FILE* stream)
 void pipe_reader(FILE* stream)
 {
 	char buffer[10];
+	//create the shared memory segment with this key
+	shmid = shmget(key,getpagesize(),IPC_CREAT|0666);
+	if(shmid<0)
+	{
+		perror("failure in shmget");
+		exit(-1);
+	}
+	char* shared_memory_b = shmat(shmid,NULL,0);
+
 	while (!ferror(stream) && fgets(buffer, sizeof (buffer), stream) != NULL) {
 		int number = atoi(buffer);
 		if (number > 100) {
-			// sem_wait(&sem);
-			// printf("> 100\n");
+			sprintf(shared_memory_b, ">100");
 			sem_post(sem);
 		} else {
 			printf("< 100\n");
 		}
-		// printf("This number was readed from pipe: %d\n", number);
-		// fputs(buffer, stdout);
 	}
 }
