31cdb852510cf667147d11173d547765b1070a97
Roman Toropov <toropov.r@gmail.com>
2017-06-12 23:31:55 +0300

    I hope it's finished

--- task3/main.c
+++ task3/main.c
@@ -17,20 +17,25 @@
 sem_t *sem;
 key_t key = 1000;
 int shmid;
-char SEM_NAME[]= "test";
-void pipe_writer (FILE* stream);
-void pipe_reader(FILE* stream);
-void *Thread1(void *arg);
-void *Thread2(void *arg);
+char SEM_NAME[]= "msg_sem";
+pthread_t thread_c_1,thread_c_2;
+sig_atomic_t termination;
+pid_t pid_C;
+void *Thread_C1(void *arg);
+void *Thread_C2(void *arg);
 void handler(int signal_number);
 
 int main() {
 	int fds[2];
 	pid_t pid, pid2;
-	pipe(fds);
+	//Creating a pipe
+    int result = pipe (fds);
+    if (result < 0) {
+        //failure in creating a pipe
+        perror("pipe");
+        exit (1);
+    }
   	
-  	
-
 	sem = sem_open(SEM_NAME,O_CREAT,0644,0);
 	 if(sem == SEM_FAILED)
     {
@@ -44,94 +49,85 @@ int main() {
 		pid2 = fork();
 		if (pid2 == 0){
 			/********** Process C **********/
-			pthread_t thread_c_1,thread_c_2;
+			pid_C = getpid();
 			int number = 0;
-
 			//create two threads to work on these semaphores
-			if(pthread_create(&thread_c_1, NULL,Thread1, (void* )&number))
+			if(pthread_create(&thread_c_1, NULL,Thread_C1, (void* )&number))
 			{
-				printf("\n ERROR creating thread 1");
+				perror("ERROR creating thread 1");
 				exit(1);
 			}
-			if(pthread_create(&thread_c_2, NULL,Thread2, (void* )&number))
+			if(pthread_create(&thread_c_2, NULL,Thread_C2, (void* )&number))
 			{
-				printf("\n ERROR creating thread 2");
+				perror("ERROR creating thread 2");
 				exit(1);
 			}
 			//waiting on these threads to complete
-			// pthread_join(thread_c_1, NULL);
-			// pthread_join(thread_c_2, NULL);
-
-
+			pthread_join(thread_c_1, NULL);
+			pthread_join(thread_c_2, NULL);
 			while(1){
-			sleep(11);
+			sleep(1);
 			/********** Process C **********/
 			}
 		} else {
 		/********** Process B **********/
-
 		FILE *stream;
 		struct sigaction sa;
 		memset(&sa, 0, sizeof(sa));
  		sa.sa_handler = &handler;
  		sigaction(SIGUSR1, &sa, NULL);
 		close(fds[1]);
-		stream = fdopen(fds[0], "r");
-		pipe_reader(stream);
-		close(fds[0]);
+		char buffer[10];	
+		//create the shared memory segment with this key
+		shmid = shmget(key,getpagesize(),IPC_CREAT|0666);
+		if(shmid<0)
+		{
+			perror("failure in shmget");
+			exit(-1);
+		}
+		int* shared_memory_b = shmat(shmid,NULL,0);
+		int number = 0;
+		while(1) {
+			read(fds[0], &number, sizeof(int));
+			number *= number;
+			memcpy(shared_memory_b, &number, sizeof(number));
+			sem_post(sem);
+			if (termination) {
+				sem_close(sem);
+				sem_unlink(SEM_NAME);
+				shmctl(shmid, IPC_RMID, NULL);
+				pthread_cancel(thread_c_1);
+				pthread_cancel(thread_c_2);
+				kill(pid_C, SIGTERM);
+				kill(getppid(), SIGTERM);
+				kill(getpid(), SIGTERM);
+			}
+		}
+		exit (0);
 		/********** Process B **********/
 		}
 	} else {
 		/********** Process A **********/
-		FILE* stream;
+		char line[10];
+		int number = 0;
+
 		close(fds[0]);
-		stream = fdopen(fds[1], "w");
 		printf("Enter the number, please:\n");
-		pipe_writer(stream);
-		close(fds[1]);
+		while (fgets(line, sizeof(line), stdin) != 0) {
+			if (sscanf(line, "%d", &number) == 1) {
+				write(fds[1], &number, sizeof(int));
+			} else {
+				printf("It's not a number, try again:\n");
+			}
+		}
+		exit (0);
 		/********** Process A **********/
 	}
 return 0;
 }
 
-void pipe_writer (FILE* stream)
-{
-    char line[10];
-    int number = 0;
 
-    while (fgets(line, sizeof(line), stdin) != 0)
-    {
-        if (sscanf(line, "%d", &number) == 1){
-        	fprintf(stream, "%d\n", number);
-			fflush(stream);
-		} else {
-			printf("It's not a number, try again:\n");
-		}
-    }
-}
-
-void pipe_reader(FILE* stream)
-{
-	char buffer[10];
-	//create the shared memory segment with this key
-	shmid = shmget(key,getpagesize(),IPC_CREAT|0666);
-	if(shmid<0)
-	{
-		perror("failure in shmget");
-		exit(-1);
-	}
-	int* shared_memory_b = shmat(shmid,NULL,0);
-
-	while (!ferror(stream) && fgets(buffer, sizeof (buffer), stream) != NULL) {
-		int number = atoi(buffer);
-		number *= number;
-		memcpy(shared_memory_b, &number, sizeof(number));
-		sem_post(sem);
-	}
-}
-
-
-void *Thread1(void *arg)
+void *Thread_C1(void *arg)
 {
 	int *readed_number;
 	readed_number = (int*)arg;
@@ -149,14 +145,13 @@ void *Thread1(void *arg)
 	while(1) {
 			if (sem_wait(sem) == 0){	
 			memcpy(readed_number, shared_memory_c, sizeof(int));
-			printf("Readed from shared memory number: %d\n", *readed_number);
 			if (*readed_number > 100) 
 				kill(getppid(), SIGUSR1);
 		}
 	}
 }
 
-void *Thread2(void *arg)
+void *Thread_C2(void *arg)
 {
 	int *number = 0;
 	number = (int*)arg;
@@ -173,5 +168,5 @@ void *Thread2(void *arg)
 
 void handler(int signal_number) 
 {
-	printf("signal received\n");
+	termination = 1;
 }
