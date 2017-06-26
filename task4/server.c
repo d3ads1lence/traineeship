4a178ca15cb0a7e7fd9a803fab2f36aed8b0053d
Roman Toropov <toropov.r@gmail.com>
2017-06-26 22:31:38 +0300

    Add a server prototype

--- task4/server.c
+++ task4/server.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "settings.h"
 
 int main (void)
 {
-	printf("server\n");
-	return 0;
+	int socket_fd, client_socket_fd, client_adr_len, read_size;
+	struct sockaddr_in server, client;
+	char client_message[2000];
+	
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_fd < 0) 
+    	perror("ERROR opening socket");
+    
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORT);
+    
+    if( bind(socket_fd,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("ERROR bind failed");
+        return 1;
+    }
+    listen(socket_fd, 128);
+    printf("Wainting connections.\n");
+
+    client_adr_len = sizeof(struct sockaddr_in);
+
+	client_socket_fd = accept(socket_fd, (struct sockaddr *)&client, 
+										(socklen_t*)&client_adr_len);
+	if (client_socket_fd < 0){
+		perror("ERROR accept failed");
+        return 1;
+	} else {
+		printf("Client connected\n");
+	}
+
+    while((read_size = recv(client_socket_fd , client_message , 2000 , 0)) > 0 )
+    {
+    	printf("%s", client_message);
+    }
+
+    if(read_size == 0)
+    {
+		printf("Client disconnected\n");
+    }
+    else if(read_size == -1)
+    {
+		perror("ERROR recv failed");
+    }
+     
+    return 0;
 }
+
---
+++ task4/settings.h
@@ -0,0 +1,6 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#define PORT    25005
+
+#endif /* SETTINGS_H */
