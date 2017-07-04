83cec32ed60d694d01bbd7537c24d4685ce1b29e
Roman Toropov <toropov.r@gmail.com>
2017-07-05 00:08:56 +0300

    Add non-blocking mode in client

--- task4/client.c
+++ task4/client.c
@@ -5,19 +5,29 @@
 #include <sys/un.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <errno.h>
 #include "settings.h"
+
+#define BUFF_SIZE 4096
+
+static int receive_all(int sock);
 
 int main (int argc, char* const argv[])
 {
 	int socket_fd;
 	struct sockaddr_in client;
-	char message[1000], received_msg[1000];
+	char message[BUFF_SIZE], received_msg[BUFF_SIZE];
 	
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (socket_fd == -1){
 		perror("ERROR coldn't create socket\n");
 	}
 	
+	struct timeval tv;
+	tv.tv_sec = 0;  
+	tv.tv_usec = 500; 
+	setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv,sizeof(struct timeval));
+
 	client.sin_addr.s_addr = inet_addr("127.0.0.1");
 	client.sin_family = AF_INET;
 	client.sin_port = htons(PORT);
@@ -28,25 +38,34 @@ int main (int argc, char* const argv[])
 	}
 	
 	printf("Connected.\n");
-	
-	while(1){
-		memset(message, 0x00, 1000);
-		while (1){
-			printf("You: ");
-			if (fgets(message, 1000, stdin) != NULL){
-				if(send(socket_fd, message, strlen(message), 0) < 0){
-            	perror("ERROR Send filed\n");
-            	return 1;
-        		}
-			}
-			int readed_data = recv(socket_fd, received_msg, 1000, 0);
-			if (readed_data < 0) {
-				printf("error\n");
-			} else if (readed_data > 0) {
-				printf("%s", received_msg);
+	while (1){
+		printf("Enter the command: ");
+		if (fgets(message, BUFF_SIZE, stdin) != NULL){
+			if(send(socket_fd, message, strlen(message), 0) < 0){
+			perror("ERROR Send filed\n");
+			return 1;
 			}
 		}
+	receive_all(socket_fd);
 	}
 	close(socket_fd);
 	return 0;
+}
+
+static int receive_all(int sock)
+{
+int size_recv , total_size= 0;
+char chunk[BUFF_SIZE];
+
+while(1) {
+	memset(chunk ,0 , BUFF_SIZE);  //clear the variable
+	if((size_recv =  recv(sock , chunk , BUFF_SIZE , 0)) < 0) {
+		break;
+	} else {
+		total_size += size_recv;
+		printf("%s", chunk);
+	}
+}
+
+return total_size;
 }
