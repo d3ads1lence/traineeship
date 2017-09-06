2b72ef88f28cd7b49e4040b2ba92c6b363ca8cc2
Roman Toropov <toropov.r@gmail.com>
2017-09-06 22:44:15 +0300

    Add output to file

--- Bash.Find_dups/find_dups.sh
+++ Bash.Find_dups/find_dups.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
 temp_file=$(mktemp)
+rm_dups=$1/rm_dups.sh
 
+echo "#!/bin/sh" > "$rm_dups"
 find $1 -type f | while read fname; do
 if ! grep -q -x "$(basename "$fname")" ${temp_file}
 	then
 		if [ $(find $1 -name "$(basename "$fname")" | wc -l) -gt 1 ]
   		then
-  		echo "----------$(basename "$fname")----------"
-  		find $1 -name "$(basename "$fname")"
+  		echo "#----------$(basename "$fname")----------" >> "$rm_dups"
+		find $1 -name "$(basename "$fname")" | while read dup_name; do
+			echo -n "#rm " >> "$rm_dups"
+			echo "$dup_name" >> "$rm_dups"
+		done
 		echo "$(basename "$fname")" >> ${temp_file}
 		fi
 fi
